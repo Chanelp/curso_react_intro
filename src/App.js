f32e5f15bdf0bd5d245559cb1c299bce6e9778bc
chanelp <mariannissanchez27@gmail.com>
2024-03-01 20:36:44 -0400

    Completando y eliminando TODOs

--- src/App.js
+++ src/App.js
@@ -25,6 +25,22 @@ function App() {
 
   const searchedTodos = todos.filter(todo => (todo.text.toLowerCase().includes(searchValue.toLocaleLowerCase())));
 
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
+
   return (
     <>
       <TodoCounter completed={todosCompleted} total={totalTodos} />
@@ -32,7 +48,12 @@ function App() {
 
       <TodoList>
         {searchedTodos.map((todo, index) => (
-          <TodoItem key={index} text={todo.text} completed={todo.completed}></TodoItem>
+          <TodoItem 
+            key={index} 
+            text={todo.text} 
+            completed={todo.completed} 
+            onComplete={() => completeTodo(todo.text)} 
+            onDelete={() => deleteTodo(todo.text)} />
         ))}
       </TodoList>
 
