cd48227a98a5d713409b4da28a2eb6aeedd04b84
chanelp <mariannissanchez27@gmail.com>
2024-03-02 00:40:18 -0400

    Iconos: Líbrerías y SVG

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import './TodoItem.css'
+import {BsCheck} from 'react-icons/bs'
+import { BsFillXCircleFill } from "react-icons/bs"
+
 
 function TodoItem(props) {
   return (
       <li className='TodoItem'>
-        <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
-              onClick={props.onComplete}>
-          ✅
+        <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`} onClick={props.onComplete}>
+          <BsCheck/>
         </span>
+
         <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
           {props.text}
         </p>
+
         <span className='Icon Icon-delete' onClick={props.onDelete}>
-          ⛔
+          <BsFillXCircleFill/>
         </span>
       </li>
   )
